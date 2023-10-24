0e149f02505c3f248276f6fffc871d2c7a45e3b2
Dimitri Avtenyev <dimitri.avtenyev@gmail.com>
2023-10-24 11:30:44 +0000

    cleanup

--- expanse-records-app/src/apollo-client.ts
+++ expanse-records-app/src/apollo-client.ts
@@ -3,12 +3,11 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 const createApolloClient = () => {
   
 let uri:string = process.env.NODE_ENV === "production" ?
-  `${process.env.CMS_URL}/graphql` : "http://host.docker.internal:1337/graphql";
-
+  `${process.env.CMS_URL}/graphql` : 
+  "http://host.docker.internal:1337/graphql";
+  
   return new ApolloClient({
     link: new HttpLink({
-      // http://host.docker.internal
-      
       uri: uri,
       headers: {
         Authorization: `Bearer ${process.env.CMS_TOKEN}`
